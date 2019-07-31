23917027c534c5f531434b3bba11e15df65ad814
ErickJoestar <erickborquez@gmail.com>
2019-07-31 00:12:21 -0600

    build added

---
+++ docs/precache-manifest.4aa63874c56fe03d0e2933ab9cda63a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85381f10e2eb510dda42df66f602f78f",
+    "url": "/react-matemagico/index.html"
+  },
+  {
+    "revision": "917305f3b27adac4831a",
+    "url": "/react-matemagico/static/css/main.76c72923.chunk.css"
+  },
+  {
+    "revision": "7dfde9ba5ead16bfb639",
+    "url": "/react-matemagico/static/js/2.71e00c83.chunk.js"
+  },
+  {
+    "revision": "917305f3b27adac4831a",
+    "url": "/react-matemagico/static/js/main.38646e5d.chunk.js"
+  },
+  {
+    "revision": "ae1e70348f73a0d05dda",
+    "url": "/react-matemagico/static/js/runtime~main.055bd262.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77b537fbee79019fa289540bcc9ea79f.js"
+  "/react-matemagico/precache-manifest.4aa63874c56fe03d0e2933ab9cda63a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-matemagico/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
